bdf5af0588bbb6e87b182c10b250ba6c135e4f0f
John Hilts <johnhilts@live.com>
2016-06-21 22:00:06 -0700

    Added more bootstrap
    also added another list shown side by side

--- Web/react/app/components/ListHelpers.js
+++ Web/react/app/components/ListHelpers.js
@@ -6,7 +6,7 @@ function ItemHelper(props) {
     helperInfo = helperInfo.substring(0, 17) + '...';
   }
   return (
-    <li>
+    <li className="list-group-item">
       {helperInfo.substring(0, 20)}
     </li>
   )
@@ -16,15 +16,32 @@ function ListHelpers(props){
   var helpers = props.helpers;
   if (helpers) {
     return (
-      <div>
-        Helpers you have previously added:<br />
-        <ul>
-          {Object.keys(helpers).map(function (key) {
-            return <ItemHelper key={key} helper={helpers[key]}  />
-          })}
-        </ul>
-    </div>
-  )
+      <div className="row">
+        <div className="panel panel-default col-sm-5">
+          <div className="panel-heading">Helpers you have previously added:</div>
+          <div className="panel-body">
+            <p>Please add up to 3 helpers here</p>
+          </div>
+          <ul className="list-group">
+            {Object.keys(helpers).map(function (key) {
+              return <ItemHelper key={key} helper={helpers[key]}  />
+            })}
+          </ul>
+        </div>
+        <div className="col-sm-1">&nbsp;</div>
+        <div className="panel panel-default col-sm-5">
+          <div className="panel-heading">Red Helper</div>
+          <div className="panel-body">
+            <p>Please pre-select a helper for red alerts by clicking on 1 of the numbers in the list on the left</p>
+          </div>
+          <ul className="list-group">
+            {Object.keys(helpers).map(function (key) {
+              return <ItemHelper key={key} helper={helpers[key]}  />
+            })}
+          </ul>
+        </div>
+      </div>
+    )
   } else {
     return <div>Please add up to 3 helpers here</div>
   }
