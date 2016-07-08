5f271f66df3a577383e2d168e09ef8919a578128
John Hilts <johnhilts@live.com>
2016-07-08 11:34:39 -0700

    refactored helper form component
    extract method for all the assignments
    added readOnly to get rid of warning messages in console

--- Web/react/app/components/HelperForm.js
+++ Web/react/app/components/HelperForm.js
@@ -1,41 +1,55 @@
 import React from 'react';
 
-export default function HelperForm(props) {
+const getFormInfo = (props) => {
   let helper = (props.helper ? Object.assign({}, props.helper) : ({'theName': props.theName, 'callNumber': props.callNumber, 'isRed': props.isRed,
     'isYellow': props.isYellow, }));
-  let hiddenHelperKey = <input type="hidden" value={helper.key} />
-  let helperIndex = props.helper ? props.helper.key : -1;
-  let isRedOnChange = !props.isAdd && props.onUpdateIsRed ? props.onUpdateIsRed.bind(null, helperIndex) : null;
-  let isRedId = "isRed" + (!props.isAdd ? helper.key : 0).toString();
-  let isYellowOnChange = !props.isAdd && props.onUpdateIsYellow ? props.onUpdateIsYellow.bind(null, helperIndex) : null;
-  let isYellowId = "isYellow" + (!props.isAdd ? helper.key : 0).toString();
   let buttonText = props.isAdd ? "Add" : "Update";
-  let addUpdateButton = props.blockEdit
-    ? <span />
-    : <input type="submit" value={buttonText} className="btn btn-info" style={{marginLeft:10, marginTop: 10, marginBottom: 10, marginRight: 10, }} />;
-  let removeButton = !props.isAdd && props.onDeleteHelper
-    ? <input type="button" value="Delete" className="btn btn-danger" onClick={props.onDeleteHelper.bind(null, helperIndex)}
-        style={{marginLeft:10, marginTop: 10, marginBottom: 10, marginRight: 10, }} />
-    : <span />;
-  let closeLink = !props.isAdd
-    ? <a onClick={props.onHelperShowOnly.bind(null, helperIndex)} style={{float: 'right',}}>close</a>
-    : <span />
+  let helperIndex = props.helper ? props.helper.key : -1;
+  let readOnly = !props.onSubmit;
+  return (
+    {
+      helper: helper,
+      hiddenHelperKey: <input type="hidden" value={helper.key} />,
+      helperIndex: helperIndex,
+      readOnly: readOnly,
+      isRedOnChange: !props.isAdd && !readOnly ? props.onUpdateIsRed.bind(null, helperIndex) : null,
+      isRedId: "isRed" + (!props.isAdd ? helper.key : 0).toString(),
+      isYellowOnChange: !props.isAdd && !readOnly ? props.onUpdateIsYellow.bind(null, helperIndex) : null,
+      isYellowId: "isYellow" + (!props.isAdd ? helper.key : 0).toString(),
+      buttonText: buttonText,
+      addUpdateButton: props.blockEdit
+        ? <span />
+        : <input type="submit" value={buttonText} className="btn btn-info" style={{marginLeft:10, marginTop: 10, marginBottom: 10, marginRight: 10, }} />,
+      removeButton: !props.isAdd && !readOnly
+        ? <input type="button" value="Delete" className="btn btn-danger" onClick={props.onDeleteHelper.bind(null, helperIndex)}
+            style={{marginLeft:10, marginTop: 10, marginBottom: 10, marginRight: 10, }} />
+        : <span />,
+      closeLink: !props.isAdd
+        ? <a onClick={props.onHelperShowOnly.bind(null, helperIndex)} style={{float: 'right',}}>close</a>
+        : <span />,
+    }
+  )
+};
+
+export default function HelperForm(props) {
+  let formInfo = getFormInfo(props);
+  let helper = formInfo.helper;
   return (
       <div>
-        {closeLink}
+        {formInfo.closeLink}
         <form onSubmit={props.onSubmit}>
           <input defaultValue={helper.theName} onChange={props.onUpdateTheName} data-helper-key={helper.key} placeholder="enter a name" /><br />
           <br />
           <input defaultValue={helper.callNumber} onChange={props.onUpdateCallNumber} data-helper-key={helper.key} placeholder="enter number" />
           <br />
-          <label htmlFor={isRedId}>Send Red Alert</label>&nbsp;
-          <input id={isRedId} type="checkbox" checked={helper.isRed} onChange={isRedOnChange} />
+          <label htmlFor={formInfo.isRedId}>Send Red Alert</label>&nbsp;
+          <input id={formInfo.isRedId} type="checkbox" checked={helper.isRed} onChange={formInfo.isRedOnChange} readOnly={formInfo.readOnly} />
           &nbsp;&nbsp;
-          <label htmlFor={isYellowId}>Send Yellow Alert</label>&nbsp;
-          <input id={isYellowId} type="checkbox" checked={helper.isYellow} onChange={isYellowOnChange} />
-          {hiddenHelperKey}
-          {addUpdateButton}
-          {removeButton}
+          <label htmlFor={formInfo.isYellowId}>Send Yellow Alert</label>&nbsp;
+          <input id={formInfo.isYellowId} type="checkbox" checked={helper.isYellow} onChange={formInfo.isYellowOnChange} readOnly={formInfo.readOnly} />
+          {formInfo.hiddenHelperKey}
+          {formInfo.addUpdateButton}
+          {formInfo.removeButton}
         </form>
       </div>
   )
